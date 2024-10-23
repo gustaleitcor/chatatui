9ee7dd6142e9657d8701d77fb72c9ac3f6f47d29
gustaleitcor <gustavo.leitecordeiro@gmail.com>
2024-10-23 07:57:26 -0300

    using procedure

--- crudbd/migrations/2024-10-22-234000_add_trigger_increase_bill/up.sql
+++ crudbd/migrations/2024-10-22-234000_add_trigger_increase_bill/up.sql
@@ -4,17 +4,12 @@ RETURNS TRIGGER AS $$
 DECLARE
     user_id_var INT;
 BEGIN
-    -- Retrieve user_id from participants
     SELECT user_id INTO user_id_var
     FROM participants
     WHERE id = NEW.participant_id;
 
-    -- Call the function to increase the user's bill
-    UPDATE users
-    SET bill = bill + 0.5
-    WHERE id = user_id_var;
+    PERFORM increase_user_bill(user_id_var);
 
-    -- Return the new row (even though not used in AFTER triggers)
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
